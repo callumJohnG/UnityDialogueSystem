c6a4413da8b57a333e66e86782d339b2f5a05bc7
callum <gillespie.14.callum@gmail.com>
2022-11-26 15:06:44 -0300

    Updates from external working
    Reformatted documentation + Readme + new dialogue actor functionality (repeatLastDialogue toggle)

--- Assets/DialogueSystem/Scripts/Main Scripts/DialogueActor.cs
+++ Assets/DialogueSystem/Scripts/Main Scripts/DialogueActor.cs
@@ -7,6 +7,7 @@ public class DialogueActor : MonoBehaviour
 {
 
     [SerializeField] private List<TextAsset> dialogueList;
+    [SerializeField] private bool repeatLastDialouge = true;
 
     [field:SerializeField] public Sprite actorSprite {get; private set;}
     [field:SerializeField] public Color actorColor {get; private set;}
@@ -20,18 +21,34 @@ public class DialogueActor : MonoBehaviour
 
 
     public void StartDialogue(){
+        //If we have run out of dialouge, then keep repeating the last dialogue if repeatLastDialogue is true
+        if(!CheckHasDialogueRemaining())return;
+        if(dialogueIndex >= dialogueList.Count){
+            dialogueIndex = dialogueList.Count -1;
+        }
+
+        
         OnDialogueStart.Invoke();
 
         DialogueBrain.Instance.StartDialogue(this, dialogueList[dialogueIndex]);
 
-
-        if(dialogueIndex < dialogueList.Count - 1){
-            dialogueIndex++;
-        }
+        dialogueIndex++;
     }
 
     public void EndDialogue(){
         OnDialogueEnd.Invoke();
     }
 
+    //Returns whether or not this actor has dialogue remaining in their list
+    //Accounts for repeatLastDialogue
+    public bool CheckHasDialogueRemaining(){
+        if(dialogueIndex >= dialogueList.Count){
+            if(repeatLastDialouge){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
